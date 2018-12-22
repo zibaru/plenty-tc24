a0a531c371c029219568147883014df1d27875ba
Ridvan Koc <ridvankoc>
2018-12-22 15:30:48 +0100

    eventdispatcher Test
    Test to overwrite the current SingleItem.twig

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -6,7 +6,13 @@ use Plenty\Plugin\ServiceProvider;
 
 class ThemeServiceProvider extends ServiceProvider
 {
-
+  $eventDispatcher->listen('IO.Component.Import', function (ComponentContainer $container)
+   	 {
+    if ($container->getOriginComponentTemplate()=='Ceres::Item.Components.SingleItem')
+   			 {
+    $container->setNewComponentTemplate('Theme::Item.SingleItem');
+   			 }
+   }, self::PRIORITY);
   /**
   * Register the service provider.
   */

75bbd9a9539dc70a8aba31d84fbdc3931033f5e1
Ridvan Koc <ridvankoc>
2018-12-22 15:33:14 +0100

    Update ThemeServiceProvider.php

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -6,18 +6,18 @@ use Plenty\Plugin\ServiceProvider;
 
 class ThemeServiceProvider extends ServiceProvider
 {
-  $eventDispatcher->listen('IO.Component.Import', function (ComponentContainer $container)
-   	 {
-    if ($container->getOriginComponentTemplate()=='Ceres::Item.Components.SingleItem')
-   			 {
-    $container->setNewComponentTemplate('Theme::Item.SingleItem');
-   			 }
-   }, self::PRIORITY);
+
   /**
   * Register the service provider.
   */
   public function register()
   {
-
+    $eventDispatcher->listen('IO.Component.Import', function (ComponentContainer $container)
+     	 {
+      if ($container->getOriginComponentTemplate()=='Ceres::Item.Components.SingleItem')
+     			 {
+      $container->setNewComponentTemplate('Theme::Item.SingleItem');
+     			 }
+     }, self::PRIORITY);
   }
 }
